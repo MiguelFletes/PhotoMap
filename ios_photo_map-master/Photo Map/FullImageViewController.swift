91f48970e45f39120f38be0121ab7c9040b7ee31
MiguelFletes <mfletes@csumb.edu>
2018-03-10 00:46:41 -0800

    finished milestone 1 & 2

---
+++ ios_photo_map-master/Photo Map/FullImageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FullImageViewController.swift
+//  Photo Map
+//
+//  Created by Nicholas Aiwazian on 10/15/15.
+//  Copyright © 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class FullImageViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //one degree of latitude is approximately 111 kilometers (69 miles) at all times.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   
+}
